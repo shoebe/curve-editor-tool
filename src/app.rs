bae3bbb8cc084f067ed9eba9519a1f287659ff1f
shoebe <smarleau.2019@gmail.com>
2024-09-14 19:11:05 -0400

    remove code

--- src/app.rs
+++ src/app.rs
@@ -141,17 +141,3 @@ impl eframe::App for TemplateApp {
         });
     }
 }
-
-fn powered_by_egui_and_eframe(ui: &mut egui::Ui) {
-    ui.horizontal(|ui| {
-        ui.spacing_mut().item_spacing.x = 0.0;
-        ui.label("Powered by ");
-        ui.hyperlink_to("egui", "https://github.com/emilk/egui");
-        ui.label(" and ");
-        ui.hyperlink_to(
-            "eframe",
-            "https://github.com/emilk/egui/tree/master/crates/eframe",
-        );
-        ui.label(".");
-    });
-}
